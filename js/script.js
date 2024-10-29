b2632cfe8fb89de3593af580852f74dadbf40483
simonesbds <simonesaba.disciullo@gmail.com>
2024-10-29 22:03:38 +0100

    Basi pagine HTML pronte. File CSS generale per tutte le pagine. Bootstrap integrato. Piccolo script JS per i bottoni del footer.

---
+++ js/script.js
@@ -0,0 +1,7 @@
+document.querySelector('.shuffle-btn').addEventListener('click', function() {
+    this.classList.toggle('text-success');
+});
+
+document.querySelector('.repeat-btn').addEventListener('click', function() {
+    this.classList.toggle('text-success');
+});
